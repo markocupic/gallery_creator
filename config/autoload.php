ee74b409404f54e6131b2cccf43c4dd2ca208de7
Marko Cupic <m.cupic@gmx.ch>
2013-02-17 23:00:23 +0100

    code beautifying

--- config/autoload.php
+++ config/autoload.php
@@ -10,7 +10,6 @@
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
 
-
 /**
  * Register the namespaces
  */
@@ -19,32 +18,30 @@ ClassLoader::addNamespaces(array
        'GalleryCreator',
 ));
 
-
 /**
  * Register the classes
  */
 ClassLoader::addClasses(array(
-	// Helpers
-	'GalleryCreator\GcHelpers' 				=> 'system/modules/gallery_creator/classes/GcHelpers.php',
-	'GalleryCreator\DisplayGallery' 			=> 'system/modules/gallery_creator/classes/DisplayGallery.php',
+       // Helpers
+       'GalleryCreator\GcHelpers'                       => 'system/modules/gallery_creator/classes/GcHelpers.php',
+       'GalleryCreator\DisplayGallery'                  => 'system/modules/gallery_creator/classes/DisplayGallery.php',
 
-	// Modules
-	'GalleryCreator\ContentDisplayGallery' 		=> 'system/modules/gallery_creator/modules/ContentDisplayGallery.php',
-	'GalleryCreator\ModuleDisplayGallery' 		=> 'system/modules/gallery_creator/modules/ModuleDisplayGallery.php',
+       // Modules
+       'GalleryCreator\ContentDisplayGallery'           => 'system/modules/gallery_creator/modules/ContentDisplayGallery.php',
+       'GalleryCreator\ModuleDisplayGallery'            => 'system/modules/gallery_creator/modules/ModuleDisplayGallery.php',
 
-	//Models
-	'GalleryCreator\GalleryCreatorAlbumsModel' 	=> 'system/modules/gallery_creator/models/GalleryCreatorAlbumsModel.php',
-	'GalleryCreator\GalleryCreatorPicturesModel' 	=> 'system/modules/gallery_creator/models/GalleryCreatorPicturesModel.php'
+       //Models
+       'GalleryCreator\GalleryCreatorAlbumsModel'       => 'system/modules/gallery_creator/models/GalleryCreatorAlbumsModel.php',
+       'GalleryCreator\GalleryCreatorPicturesModel'     => 'system/modules/gallery_creator/models/GalleryCreatorPicturesModel.php'
 ));
-
 
 /**
  * Register the templates
  */
 TemplateLoader::addFiles(array(
-'be_gc_jumploader'          => 'system/modules/gallery_creator/templates',
-'ce_gc_default' 		=> 'system/modules/gallery_creator/templates', 
-'ce_gc_jquery_galleria'     => 'system/modules/gallery_creator/templates',
-'ce_gc_jw_imagerotator'     => 'system/modules/gallery_creator/templates',
-'ce_gc_lightbox'            => 'system/modules/gallery_creator/templates'
+       'be_gc_jumploader'          => 'system/modules/gallery_creator/templates',
+       'ce_gc_default'             => 'system/modules/gallery_creator/templates',
+       'ce_gc_jquery_galleria'     => 'system/modules/gallery_creator/templates',
+       'ce_gc_jw_imagerotator'     => 'system/modules/gallery_creator/templates',
+       'ce_gc_lightbox'            => 'system/modules/gallery_creator/templates'
 ));		
