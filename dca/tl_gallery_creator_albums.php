018215df8fc0dae9ae469638248c7759867812d3
Marko Cupic <m.cupic@gmx.ch>
2015-01-24 22:04:25 +0100

    reorganized tl_gallery_creator_pictures.onCutCb()

--- dca/tl_gallery_creator_albums.php
+++ dca/tl_gallery_creator_albums.php
@@ -147,7 +147,7 @@ $GLOBALS['TL_DCA']['tl_gallery_creator_albums'] = array(
             'sql'        => "int(10) unsigned NOT NULL default '0'",
             'relation'   => array('type' => 'hasOne', 'load' => 'eager')
         ),
-        'assignedDir'          => array(
+        'assignedDir'   => array(
             'label'     => &$GLOBALS['TL_LANG']['tl_gallery_creator_albums']['assignedDir'],
             'exclude'   => true,
             'inputType' => 'fileTree',
