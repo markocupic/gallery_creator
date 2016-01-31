9d4e768ea9adc4695944fd9810f20b1dd40d9e11
Marko Cupic <m.cupic@gmx.ch>
2016-01-31 21:29:15 +0100

    removed abstract class "GalleryCreator"

--- config/autoload.php
+++ config/autoload.php
@@ -25,7 +25,6 @@ ClassLoader::addClasses(array
 (
 	// Src
 	'MCupic\GalleryCreator\GcHelpers'                 => 'system/modules/gallery_creator/src/classes/GcHelpers.php',
-	'MCupic\GalleryCreator\GalleryCreator'            => 'system/modules/gallery_creator/src/classes/GalleryCreator.php',
 	'MCupic\GalleryCreatorPicturesModel'              => 'system/modules/gallery_creator/src/models/GalleryCreatorPicturesModel.php',
 	'MCupic\GalleryCreatorAlbumsModel'                => 'system/modules/gallery_creator/src/models/GalleryCreatorAlbumsModel.php',
 	'MCupic\GalleryCreator\ModuleGalleryCreator'      => 'system/modules/gallery_creator/src/modules/ModuleGalleryCreator.php',
@@ -41,6 +40,5 @@ TemplateLoader::addFiles(array
 (
 	'ce_gc_default'        => 'system/modules/gallery_creator/templates',
 	'be_gc_html5_uploader' => 'system/modules/gallery_creator/templates',
-	'be_gc_jumploader'     => 'system/modules/gallery_creator/templates',
 	'ce_gc_news_default'   => 'system/modules/gallery_creator/templates',
 ));
